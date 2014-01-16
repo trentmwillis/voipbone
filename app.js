d6e373ce7a4821728554d1567dc474c7ac7d8971
Trent Willis <trentmwillis@gmail.com>
2014-01-16 16:41:07 -0500

    Most functionality is finished, got some cleaning up to do though

--- app.js
+++ app.js
@@ -1,127 +1,132 @@
 /*
- * FINISHED: Call a contact, call voicemail, delete a contact, change status of the user, change status of the user, add a contact, change user name,
- * TODO: View call history with a contact, redial a number, start a three-way phone-call, mute the phone, change the clock
+ * FINISHED: Call a contact, call voicemail, delete a contact, change status of the user, change status of the user, add a contact, change user name, start a three-way phone-call, mute the phone, View call history with a contact, redial a number 
+ * TODO: Change the clock
  */
 
 $(function() {
 
 	var contacts = [
 			{
-				"name":"Elise Van Wie",
-				"status":"available",
-				"number":"123-456-7890",
-				"callHistory":[1,2,3]
+				"name": "Elise Van Wie",
+				"status": "available",
+				"number": "123-456-7890",
+				"contactID": 1
 			},
 			{
 				"name":"Angela Niu",
 				"status":"unavailable",
-				"number":"123-444-5555"
+				"number":"123-444-5555",
+				"contactID": 2
 			},
 			{
 				"name":"Andres Hernandez",
 				"status":"available",
 				"number":"123-456-7890",
-				"callHistory":[1,2,3]
+				"contactID": 3
 			},
 			{
 				"name":"Morgan Freeman",
 				"status":"available",
 				"number":"123-456-7890",
-				"callHistory":[1,2,3]
+				"contactID": 4
 			},
 			{
 				"name":"Oli Sykes",
 				"status":"available",
 				"number":"123-456-7890",
-				"callHistory":[1,2,3]
+				"contactID": 5
 			},
 			{
 				"name":"Laura Turanchik",
 				"status":"available",
 				"number":"123-456-7890",
-				"callHistory":[1,2,3]
+				"contactID": 6
 			},
 			{
 				"name":"Daniel Rutledge",
 				"status":"available",
 				"number":"123-456-7890",
-				"callHistory":[1,2,3]
+				"contactID": 7
 			},
 			{
 				"name":"Kaitlyn Irvine",
 				"status":"available",
 				"number":"123-456-7890",
-				"callHistory":[1,2,3]
+				"contactID": 8
 			},
 			{
 				"name":"Dina-Marie Kolman",
 				"status":"available",
 				"number":"123-456-7890",
-				"callHistory":[1,2,3]
+				"contactID": 9
 			},
 			{
 				"name":"Daniel Esneul",
 				"status":"available",
 				"number":"123-456-7890",
-				"callHistory":[1,2,3]
+				"contactID": 10
 			},
 			{
 				"name":"Nicholas Bianco",
 				"status":"available",
 				"number":"123-456-7890",
-				"callHistory":[1,2,3]
+				"contactID": 11
 			},
 			{
 				"name":"Karl 'Max' Wallace",
 				"status":"available",
 				"number":"123-456-7890",
-				"callHistory":[1,2,3]
+				"contactID": 12
 			}
 		],
 		callHistory = [
 				{
-					"recipient":"Trent Willis",
-					"date":"Thursday, January 9th, 2014",
-					"time":"6:15 PM",
-					"type":"Incoming",
-					"duration":"0:53:20",
-					"callID": 1
+					"callers": [1,2],
+					"date": "Friday, January 10th, 2014",
+					"time": "6:15 PM",
+					"type": "Incoming",
+					"duration": "0:53:20"
 				},
 				{
-					"recipient":"Trent Willis",
+					"callers": [3,4,5],
 					"date":"Thursday, January 9th, 2014",
 					"time":"6:15 PM",
-					"type":"Incoming",
-					"duration":"0:53:20",
-					"callID": 2
+					"type":"Outgoing",
+					"duration":"0:53:20"
 				},
 				{
-					"recipient":"Trent Willis",
-					"date":"Thursday, January 9th, 2014",
+					"callers": [5,6],
+					"date":"Monday, January 6th, 2014",
 					"time":"6:15 PM",
 					"type":"Incoming",
-					"duration":"0:53:20",
-					"callID": 4
+					"duration":"0:53:20"
 				},
 				{
-					"recipient":"Trent Willis",
-					"date":"Thursday, January 9th, 2014",
+					"callers": [10],
+					"date":"Sunday, January 5th, 2014",
 					"time":"6:15 PM",
-					"type":"Incoming",
-					"duration":"0:53:20",
-					"callID": 2
+					"type":"Outgoing",
+					"duration":"0:53:20"
 				},
 				{
-					"recipient":"Trent Willis",
-					"date":"Thursday, January 9th, 2014",
+					"callers": [12],
+					"date":"Friday, January 3rd, 2014",
 					"time":"6:15 PM",
 					"type":"Incoming",
-					"duration":"0:53:20",
-					"callID": 4
+					"duration":"0:53:20"
+				},
+				{
+					"callers": [1, 4],
+					"date":"Wednesday, January 1st, 2014",
+					"time":"6:15 PM",
+					"type":"Outgoing",
+					"duration":"0:53:20"
 				}
 			],
 			inCall = false,
-			selection = null;
+			currentCall = null,
+			selection = null,
+			app;
 
 	var Contact = Backbone.Model.extend({
 		defaults: {
@@ -163,6 +168,9 @@ $(function() {
 		viewContactHistory: function() {
 			var $history = this.$('.contact-history'),
 				height;
+
+			this.getContactHistory();
+
 			if ($history.hasClass('open')) {
 				$history.animate({"height":"0px"}, 500, function() {
 					$(this).removeClass('open');
@@ -181,8 +189,27 @@ $(function() {
 				});
 			}
 		},
+		getContactHistory: function() {
+			var contactID = this.model.attributes.contactID,
+				history = _.filter(callHistory, function(call) {
+					return call.callers.indexOf(contactID) >= 0;
+				}),
+				$historyContainer = this.$('.contact-history');
+				
+				if (history.length) {
+					$historyContainer.html("");
+				} else {
+					$historyContainer.html("<div class='contact-history-entry'>No call history with " + this.model.attributes.name + "</div>");
+				}
+				
+
+			for (var i=0; i<history.length; i++) {
+				$historyContainer.append(_.template($('#contactCallTemplate').html())(history[i]));
+			}
+		},
 		callContact: function() {
-			var $callArea = $('#call-status');
+			var $callArea = $('#call-status'),
+				contactID = this.model.attributes.contactID;
 			if (inCall && !confirm("You are about to end your current call.")) {
 				return;
 			}
@@ -192,6 +219,9 @@ $(function() {
 			$callArea.addClass('active');
 			$callArea.children('.caller-id').html('<small>On the phone with</small><br>' + this.model.attributes.name);
 			inCall = true;
+			currentCall = new Call({
+				callers: contactID
+			});
 		},
 		activate: function() {
 			$('.contact-row.active').removeClass('active');
@@ -202,10 +232,40 @@ $(function() {
 
 	var CallView = Backbone.View.extend({
 		template: $("#callTemplate").html(),
+		events: {
+			'click .redial' : 'redial'
+		},
 		render: function() {
-			var tmpl = _.template(this.template);
+			var tmpl = _.template(this.template),
+				callers = this.model.attributes.callers;
+			this.model.attributes.callerNames = [];
+			for (var i=0; i<callers.length; i++) {
+				if (app) {
+					this.model.attributes.callerNames.push(_.find(app.contactPage.collection.models, function(contact) {
+						return contact.attributes.contactID === callers[i];
+					}).attributes.name);
+				}
+			}
 			this.$el.html(tmpl(this.model.toJSON()));
 			return this;
+		},
+		redial: function() {
+			var that = this,
+				caller = _.find(app.contactPage.collection.models, function(contact) {
+					return contact.attributes.contactID === that.model.attributes.callers[0];
+				});
+
+			// Start the call
+			new ContactView({
+				model: caller
+			}).callContact();
+
+			// Add any additional contacts that were a part of the call
+			if (this.model.attributes.callers.length > 1) {
+
+			}
+			currentCall = this;
+			inCall = true;
 		}
 	});
 
@@ -278,12 +338,18 @@ $(function() {
 			"click #voicemails" : "callVoicemail",
 			"click .action.delete" : "deleteContact",
 			"click #create-contact button" : "addContact",
-			"click .fa-volume-off" : "mute"
+			"click .fa-volume-off" : "mute",
+			"click .fa-volume-up" : "volumeControl",
+			"click .fa-volume-down" : "volumeControl",
+			"click #three-way" : "addCallerToCall",
+			"click #clock" : "setClock",
+			"click #redial" : "redialLastCall"
 		},
 		initialize: function() {
 			this.model = new User();
 			this.callHistoryPage = new CallCollectionView();
 			this.contactPage = new ContactCollectionView();
+			this.clock = new ClockView();
 
 			this.renderName();
 			this.renderStatus();
@@ -354,9 +420,11 @@ $(function() {
 		},
 		deleteContact: function() {
 			if (selection && confirm("Warning: you are about to delete " + selection.model.attributes.name + " from you contact list. Proceed?")) {
-				selection.model.destroy();
-				selection.remove();
-				selection = null;
+				selection.$el.animate({"height":"0px"}, 500, function() {
+					selection.model.destroy();
+					selection.remove();
+					selection = null;
+				});
 			} else if (!selection) {
 				alert("Please select a contact to delete.");
 			}
@@ -378,9 +446,113 @@ $(function() {
 			this.contactPage.render();
 		},
 		mute: function() {
-			
+			var $volumeIcons = $("#volume-actions");
+			if ($volumeIcons.hasClass("muted")) {
+				$volumeIcons.removeClass("muted");
+			} else {
+				$volumeIcons.addClass("muted");
+			}
+		},
+		addCallerToCall: function() {
+			var $callArea = $('#call-status');
+			if (!selection) {
+				alert("Please select a contact to add to the call");
+				return;
+			}
+
+			$callArea.children('.caller-id').append(", " + selection.model.attributes.name);
+		},
+		volumeControl: function() {
+			var $volumeIcons = $("#volume-actions");
+			if ($volumeIcons.hasClass("muted")) {
+				$volumeIcons.removeClass("muted");
+			}
+		},
+		renderCallTime: function() {
+			var startTime = new Date(currentCall.time).getTime(),
+				timeNow = $.now(),
+				seconds = timeNow - startTime,
+				hours = Math.floor(seconds/3600),
+				minutes, timeString;
+
+				seconds -= hours*3600;
+				minutes = seconds/60;
+				seconds -= minutes*60;
+
+				timeString = hours + ":" + minutes + ":" + seconds;
+
+				$("#current-call-time").html(timeString);
+				setTimeout(this.renderCallTime, 1000);
+		},
+		redialLastCall: function() {
+			var caller = _.find(this.contactPage.collection.models, function(contact) {
+				return contact.attributes.contactID === currentCall.attributes.callers;
+			});
+			new ContactView({
+				model: caller
+			}).callContact();
 		}
 	});
 
-	var app = new PageView();
+	var Clock = Backbone.Model.extend({
+		initialize: function() {
+			this.hoursOffset = 0;
+			this.minutesOffset = 0;
+			this.markerSwap = false;
+			this.update();
+		},
+		update: function() {
+			this.currentTime = new Date();
+			this.hours = (this.currentTime.getHours() > 12) ? this.currentTime.getHours() - 12 : this.currentTime.getHours();
+			if (this.hoursOffset) {
+				this.hours = this.hours + this.hoursOffset;
+				this.hours = (this.hours < 12) ? this.hours : this.hours - 12;
+			}
+
+			this.minutes = (this.currentTime.getMinutes() < 10) ? "0" + this.currentTime.getMinutes() : this.currentTime.getMinutes();
+			this.minutes = this.minutes + this.minutesOffset;
+			if (this.minutes > 59) {
+				this.hours++;
+				this.minutes -= 59;
+			}
+
+			this.marker = (this.currentTime.getHours() >= 12 && !this.markerSwap) ? "PM" : "AM";
+		}
+	});
+
+	var ClockView = Backbone.View.extend({
+		el: $('#clock'),
+		events: {
+			"click" : "setClock"
+		},
+		initialize: function() {
+			var that = this;
+			this.model = new Clock();
+			this.render();
+			setInterval(function() {that.render()}, 1000);
+		},
+		render: function() {
+			this.model.update();
+			this.$el.html(this.model.hours + ":" + this.model.minutes + " " + this.model.marker);
+		},
+		setClock: function() {
+			var hours = prompt("Please enter the hour:"),
+				minutes = prompt("Please enter the minutes:"),
+				marker = prompt("Please enter 'AM' or 'PM':");
+
+			if (hours !== this.model.hours) {
+				this.model.hoursOffset = hours - this.model.hours;
+			}
+
+			if (minutes !== this.model.minutes) {
+				this.model.minutesOffset = minutes - this.model.minutes;
+			}
+
+			if (marker !== this.model.marker) {
+				this.model.markerSwap = true;
+			}
+		}
+	});
+
+	app = new PageView();
 });
